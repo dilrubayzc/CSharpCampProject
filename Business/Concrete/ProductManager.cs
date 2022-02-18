e00648e756013a156259d39671a326017bd6643e
Dilruba Yazıcı (DGPAYSIT) <dilruba.yazici@dgpaysit.com>
2022-02-18 09:39:57 +0300

    Web Api Added

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -32,7 +32,7 @@ namespace Business.Concrete
         public IDataResult<List<Product>> GetAll()
         {
             //iş kodları
-            if(DateTime.Now.Hour==22)
+            if(DateTime.Now.Hour==23)
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             return new SuccessDataResult<List<Product>>(productDal.GetAll(),"ürünler listelendi");
         }
---
+++ WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,45 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        IProductService productService;
+
+        public ProductsController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        [HttpGet("GetProducts")]
+        public IActionResult GetProduct()
+        {
+            var result = productService.GetAll();
+            if (result.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+        [HttpGet("GetProductById")]
+        public IActionResult GetProductByd(int id)
+        {
+            var result = productService.GetById(id);
+            if (result.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+        [HttpPost("AddProduct")]
+        public IActionResult Add(Product product)
+        {
+            var result = productService.Add(product);
+            if (result.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+    }
+}
